cc3741e2a27350d29991f0b422324b61103be107
Maruf-dev <maruf123erkinov@gmail.com>
2024-05-01 10:20:56 +0500

    add accord

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,114 @@
-import { Container } from "@chakra-ui/react";
-import { Code, Stack, Text } from "@chakra-ui/react";
+import {
+  Container,
+  Code,
+  Stack,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
+import { MinusIcon, AddIcon } from "@chakra-ui/icons";
 import "./App.css";
+
+
+
+
+
 
 const App = () => {
   return (
     <>
-      <Container maxW="" bg="blue.900" color="white">
-        <Stack direction="column">
-          <Text fontSize="2xl">Start </Text>
-          <Code>
-            r_drawtracers "1"; hud_showtargetid "1"; hud_scaling "1"; fps_max
-            "500"; cl_autohelp "0"; cl_showfps "1";
-          </Code>
-
-          <Text fontSize="2xl">Cross hair</Text>
-          <Code> CSGO-SZLXx-25PxR-Yyftb-dVwjT-fxwPB</Code>
-
-          <Text fontSize="2xl">Viewmodel</Text>
-
-          <Code>
-            viewmodel_fov 68; viewmodel_offset_x 2.5; viewmodel_offset_y 0;
-            viewmodel_offset_z -1.5; viewmodel_presetpos 3; cl_usenewbob false;
-            cl_usenewbob 0;
-          </Code>
-          <Text fontSize="2xl">Binds</Text>
-          <Code>
-            alias "+jumpaction" "+jump;"; alias "+throwaction" "-attack;
-            -attack2"; alias "-jumpaction" "-jump"; bind "j"
-            "+jumpaction;+throwaction;"
-          </Code>
-          <Code>
-            bind "c" "+duck"; bind "mwheeldown" "+jump"; bind "mwheelup" "+lookatweapon";
-          </Code>
-
-          <Text fontSize="2xl">Hud</Text>
-          <Code> cl_hud_color "4";
-cl_hud_radar_scale "0.910000";
-cl_radar_always_centered "1";
-cl_radar_icon_scale_min "0.500000";
-cl_radar_scale "0.4";
-cl_radar_rotate "1";</Code>
-
-        </Stack>
+      <Container maxW="1500px" bg="blue.900" color="white">
+        <Accordion allowMultiple>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left">
+                  Start
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <Code>
+                r_drawtracers "1"; hud_showtargetid "1"; hud_scaling "1";
+                fps_max "500"; cl_autohelp "0"; cl_showfps "1";
+              </Code>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left">
+                  Cross hair
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <Code> CSGO-SZLXx-25PxR-Yyftb-dVwjT-fxwPB</Code>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left">
+                  Viewmodel
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <Code>
+                viewmodel_fov 68; viewmodel_offset_x 2.5; viewmodel_offset_y 0;
+                viewmodel_offset_z -1.5; viewmodel_presetpos 3; cl_usenewbob
+                false; cl_usenewbob 0;
+              </Code>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left">
+                  Binds
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <Code m={2}>
+                alias "+jumpaction" "+jump;"; <br /> alias "+throwaction"
+                "-attack; <br />
+                -attack2"; alias "-jumpaction" "-jump"; <br /> bind "j"
+                "+jumpaction;+throwaction;"
+              </Code>
+              <Code m={2}>
+                bind "c" "+duck"; <br /> bind "mwheeldown" "+jump"; <br /> bind
+                "mwheelup" "+lookatweapon"; <br />
+              </Code>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left">
+                  Hud
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              <Code m={2}>
+                cl_hud_color "4"; cl_hud_radar_scale "0.910000";
+                cl_radar_always_centered "1"; cl_radar_icon_scale_min
+                "0.500000"; cl_radar_scale "0.4"; cl_radar_rotate "1";
+              </Code>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
       </Container>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
+import { extendTheme } from '@chakra-ui/react'
+
+
+
+
+const theme = extendTheme({
+  config: {
+    cssVarPrefix: 'ck',
+  },
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </React.StrictMode>,
